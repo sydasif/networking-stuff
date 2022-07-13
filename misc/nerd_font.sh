e046b72b587c5fe0bd4233a23b55a9572fe0aee7
Syed Asif <76906817+sydasif@users.noreply.github.com>
2022-07-13 14:31:17 +0500

    Add files via upload
    add nerd_font.sh installation

---
+++ misc/nerd_font.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
+echo "done!"
